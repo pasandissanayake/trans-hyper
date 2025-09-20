f2f13bee2075ba54f975366ba0c932c2000a1ab4
pasandissanayake <pasandissanayake@gmail.com>
2025-09-20 15:31:16 -0400

    new scripts added

---
+++ run4.sh
@@ -0,0 +1,34 @@
+# Seed 14
+python main.py --name "t0pp-bank-n04-seed14" --cfg "cfgs/t0/n04/t0_bank.yaml" --seed 14 --group "t0pp-bank-n04" -w
+python main.py --name "t0pp-bank-n08-seed14" --cfg "cfgs/t0/n08/t0_bank.yaml" --seed 14 --group "t0pp-bank-n08" -w
+python main.py --name "t0pp-bank-n16-seed14" --cfg "cfgs/t0/n16/t0_bank.yaml" --seed 14 --group "t0pp-bank-n16" -w
+python main.py --name "t0pp-bank-n32-seed14" --cfg "cfgs/t0/n32/t0_bank.yaml" --seed 14 --group "t0pp-bank-n32" -w
+python main.py --name "t0pp-bank-n64-seed14" --cfg "cfgs/t0/n64/t0_bank.yaml" --seed 14 --group "t0pp-bank-n64" -w
+
+# Seed 26
+python main.py --name "t0pp-bank-n04-seed26" --cfg "cfgs/t0/n04/t0_bank.yaml" --seed 26 --group "t0pp-bank-n04" -w
+python main.py --name "t0pp-bank-n08-seed26" --cfg "cfgs/t0/n08/t0_bank.yaml" --seed 26 --group "t0pp-bank-n08" -w
+python main.py --name "t0pp-bank-n16-seed26" --cfg "cfgs/t0/n16/t0_bank.yaml" --seed 26 --group "t0pp-bank-n16" -w
+python main.py --name "t0pp-bank-n32-seed26" --cfg "cfgs/t0/n32/t0_bank.yaml" --seed 26 --group "t0pp-bank-n32" -w
+python main.py --name "t0pp-bank-n64-seed26" --cfg "cfgs/t0/n64/t0_bank.yaml" --seed 26 --group "t0pp-bank-n64" -w
+
+# Seed 42
+python main.py --name "t0pp-bank-n04-seed42" --cfg "cfgs/t0/n04/t0_bank.yaml" --seed 42 --group "t0pp-bank-n04" -w
+python main.py --name "t0pp-bank-n08-seed42" --cfg "cfgs/t0/n08/t0_bank.yaml" --seed 42 --group "t0pp-bank-n08" -w
+python main.py --name "t0pp-bank-n16-seed42" --cfg "cfgs/t0/n16/t0_bank.yaml" --seed 42 --group "t0pp-bank-n16" -w
+python main.py --name "t0pp-bank-n32-seed42" --cfg "cfgs/t0/n32/t0_bank.yaml" --seed 42 --group "t0pp-bank-n32" -w
+python main.py --name "t0pp-bank-n64-seed42" --cfg "cfgs/t0/n64/t0_bank.yaml" --seed 42 --group "t0pp-bank-n64" -w
+
+# Seed 58
+python main.py --name "t0pp-bank-n04-seed58" --cfg "cfgs/t0/n04/t0_bank.yaml" --seed 58 --group "t0pp-bank-n04" -w
+python main.py --name "t0pp-bank-n08-seed58" --cfg "cfgs/t0/n08/t0_bank.yaml" --seed 58 --group "t0pp-bank-n08" -w
+python main.py --name "t0pp-bank-n16-seed58" --cfg "cfgs/t0/n16/t0_bank.yaml" --seed 58 --group "t0pp-bank-n16" -w
+python main.py --name "t0pp-bank-n32-seed58" --cfg "cfgs/t0/n32/t0_bank.yaml" --seed 58 --group "t0pp-bank-n32" -w
+python main.py --name "t0pp-bank-n64-seed58" --cfg "cfgs/t0/n64/t0_bank.yaml" --seed 58 --group "t0pp-bank-n64" -w
+
+# Seed 97
+python main.py --name "t0pp-bank-n04-seed97" --cfg "cfgs/t0/n04/t0_bank.yaml" --seed 97 --group "t0pp-bank-n04" -w
+python main.py --name "t0pp-bank-n08-seed97" --cfg "cfgs/t0/n08/t0_bank.yaml" --seed 97 --group "t0pp-bank-n08" -w
+python main.py --name "t0pp-bank-n16-seed97" --cfg "cfgs/t0/n16/t0_bank.yaml" --seed 97 --group "t0pp-bank-n16" -w
+python main.py --name "t0pp-bank-n32-seed97" --cfg "cfgs/t0/n32/t0_bank.yaml" --seed 97 --group "t0pp-bank-n32" -w
+python main.py --name "t0pp-bank-n64-seed97" --cfg "cfgs/t0/n64/t0_bank.yaml" --seed 97 --group "t0pp-bank-n64" -w
---
+++ run6.sh
@@ -0,0 +1,34 @@
+# Seed 14
+python main.py --name "t0pp-income-n04-seed14" --cfg "cfgs/t0/n04/t0_income.yaml" --seed 14 --group "t0pp-income-n04" -w
+python main.py --name "t0pp-income-n08-seed14" --cfg "cfgs/t0/n08/t0_income.yaml" --seed 14 --group "t0pp-income-n08" -w
+python main.py --name "t0pp-income-n16-seed14" --cfg "cfgs/t0/n16/t0_income.yaml" --seed 14 --group "t0pp-income-n16" -w
+python main.py --name "t0pp-income-n32-seed14" --cfg "cfgs/t0/n32/t0_income.yaml" --seed 14 --group "t0pp-income-n32" -w
+python main.py --name "t0pp-income-n64-seed14" --cfg "cfgs/t0/n64/t0_income.yaml" --seed 14 --group "t0pp-income-n64" -w
+
+# Seed 26
+python main.py --name "t0pp-income-n04-seed26" --cfg "cfgs/t0/n04/t0_income.yaml" --seed 26 --group "t0pp-income-n04" -w
+python main.py --name "t0pp-income-n08-seed26" --cfg "cfgs/t0/n08/t0_income.yaml" --seed 26 --group "t0pp-income-n08" -w
+python main.py --name "t0pp-income-n16-seed26" --cfg "cfgs/t0/n16/t0_income.yaml" --seed 26 --group "t0pp-income-n16" -w
+python main.py --name "t0pp-income-n32-seed26" --cfg "cfgs/t0/n32/t0_income.yaml" --seed 26 --group "t0pp-income-n32" -w
+python main.py --name "t0pp-income-n64-seed26" --cfg "cfgs/t0/n64/t0_income.yaml" --seed 26 --group "t0pp-income-n64" -w
+
+# Seed 42
+python main.py --name "t0pp-income-n04-seed42" --cfg "cfgs/t0/n04/t0_income.yaml" --seed 42 --group "t0pp-income-n04" -w
+python main.py --name "t0pp-income-n08-seed42" --cfg "cfgs/t0/n08/t0_income.yaml" --seed 42 --group "t0pp-income-n08" -w
+python main.py --name "t0pp-income-n16-seed42" --cfg "cfgs/t0/n16/t0_income.yaml" --seed 42 --group "t0pp-income-n16" -w
+python main.py --name "t0pp-income-n32-seed42" --cfg "cfgs/t0/n32/t0_income.yaml" --seed 42 --group "t0pp-income-n32" -w
+python main.py --name "t0pp-income-n64-seed42" --cfg "cfgs/t0/n64/t0_income.yaml" --seed 42 --group "t0pp-income-n64" -w
+
+# Seed 58
+python main.py --name "t0pp-income-n04-seed58" --cfg "cfgs/t0/n04/t0_income.yaml" --seed 58 --group "t0pp-income-n04" -w
+python main.py --name "t0pp-income-n08-seed58" --cfg "cfgs/t0/n08/t0_income.yaml" --seed 58 --group "t0pp-income-n08" -w
+python main.py --name "t0pp-income-n16-seed58" --cfg "cfgs/t0/n16/t0_income.yaml" --seed 58 --group "t0pp-income-n16" -w
+python main.py --name "t0pp-income-n32-seed58" --cfg "cfgs/t0/n32/t0_income.yaml" --seed 58 --group "t0pp-income-n32" -w
+python main.py --name "t0pp-income-n64-seed58" --cfg "cfgs/t0/n64/t0_income.yaml" --seed 58 --group "t0pp-income-n64" -w
+
+# Seed 97
+python main.py --name "t0pp-income-n04-seed97" --cfg "cfgs/t0/n04/t0_income.yaml" --seed 97 --group "t0pp-income-n04" -w
+python main.py --name "t0pp-income-n08-seed97" --cfg "cfgs/t0/n08/t0_income.yaml" --seed 97 --group "t0pp-income-n08" -w
+python main.py --name "t0pp-income-n16-seed97" --cfg "cfgs/t0/n16/t0_income.yaml" --seed 97 --group "t0pp-income-n16" -w
+python main.py --name "t0pp-income-n32-seed97" --cfg "cfgs/t0/n32/t0_income.yaml" --seed 97 --group "t0pp-income-n32" -w
+python main.py --name "t0pp-income-n64-seed97" --cfg "cfgs/t0/n64/t0_income.yaml" --seed 97 --group "t0pp-income-n64" -w
