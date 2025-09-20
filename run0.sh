b196c8b51438d44f4ff08c6862ae315e552cd894
pasandissanayake <pasandissanayake@gmail.com>
2025-09-20 13:11:06 -0400

    classical model cv hpo added

--- run0.sh
+++ run0.sh
@@ -1,22 +1,30 @@
-# python main.py --name "dup-tabpfn-bank-n04-seed14" --cfg "cfgs/pfn/n04/bank.yaml" --seed 14 --group "tabpfn-bank-n04" -w
-# python main.py --name "dup-tabpfn-bank-n04-seed26" --cfg "cfgs/pfn/n04/bank.yaml" --seed 26 --group "tabpfn-bank-n04" -w
-# python main.py --name "dup-tabpfn-bank-n04-seed42" --cfg "cfgs/pfn/n04/bank.yaml" --seed 42 --group "tabpfn-bank-n04" -w
+python main.py --name "tabpfn-bank-n04-seed14" --cfg "cfgs/pfn/n04/bank.yaml" --seed 14 --group "tabpfn-bank-n04" -w
+python main.py --name "tabpfn-bank-n04-seed26" --cfg "cfgs/pfn/n04/bank.yaml" --seed 26 --group "tabpfn-bank-n04" -w
+python main.py --name "tabpfn-bank-n04-seed42" --cfg "cfgs/pfn/n04/bank.yaml" --seed 42 --group "tabpfn-bank-n04" -w
+python main.py --name "tabpfn-bank-n04-seed58" --cfg "cfgs/pfn/n04/bank.yaml" --seed 58 --group "tabpfn-bank-n04" -w
+python main.py --name "tabpfn-bank-n04-seed97" --cfg "cfgs/pfn/n04/bank.yaml" --seed 97 --group "tabpfn-bank-n04" -w
 
-python main.py --name "dup-tabpfn-bank-n08-seed14" --cfg "cfgs/pfn/n08/bank.yaml" --seed 14 --group "tabpfn-bank-n08" -w
-python main.py --name "dup-tabpfn-bank-n08-seed26" --cfg "cfgs/pfn/n08/bank.yaml" --seed 26 --group "tabpfn-bank-n08" -w
-python main.py --name "dup-tabpfn-bank-n08-seed42" --cfg "cfgs/pfn/n08/bank.yaml" --seed 42 --group "tabpfn-bank-n08" -w
+python main.py --name "tabpfn-bank-n08-seed14" --cfg "cfgs/pfn/n08/bank.yaml" --seed 14 --group "tabpfn-bank-n08" -w
+python main.py --name "tabpfn-bank-n08-seed26" --cfg "cfgs/pfn/n08/bank.yaml" --seed 26 --group "tabpfn-bank-n08" -w
+python main.py --name "tabpfn-bank-n08-seed42" --cfg "cfgs/pfn/n08/bank.yaml" --seed 42 --group "tabpfn-bank-n08" -w
+python main.py --name "tabpfn-bank-n08-seed58" --cfg "cfgs/pfn/n08/bank.yaml" --seed 58 --group "tabpfn-bank-n08" -w
+python main.py --name "tabpfn-bank-n08-seed97" --cfg "cfgs/pfn/n08/bank.yaml" --seed 97 --group "tabpfn-bank-n08" -w
 
-# python main.py --name "dup-tabpfn-bank-n64-seed14" --cfg "cfgs/pfn/n64/bank.yaml" --seed 14 --group "tabpfn-bank-n64" -w
-# python main.py --name "dup-tabpfn-bank-n64-seed26" --cfg "cfgs/pfn/n64/bank.yaml" --seed 26 --group "tabpfn-bank-n64" -w
-# python main.py --name "dup-tabpfn-bank-n64-seed42" --cfg "cfgs/pfn/n64/bank.yaml" --seed 42 --group "tabpfn-bank-n64" -w
+python main.py --name "tabpfn-bank-n16-seed14" --cfg "cfgs/pfn/n16/bank.yaml" --seed 14 --group "tabpfn-bank-n16" -w
+python main.py --name "tabpfn-bank-n16-seed26" --cfg "cfgs/pfn/n16/bank.yaml" --seed 26 --group "tabpfn-bank-n16" -w
+python main.py --name "tabpfn-bank-n16-seed42" --cfg "cfgs/pfn/n16/bank.yaml" --seed 42 --group "tabpfn-bank-n16" -w
+python main.py --name "tabpfn-bank-n16-seed58" --cfg "cfgs/pfn/n16/bank.yaml" --seed 58 --group "tabpfn-bank-n16" -w
+python main.py --name "tabpfn-bank-n16-seed97" --cfg "cfgs/pfn/n16/bank.yaml" --seed 97 --group "tabpfn-bank-n16" -w
 
-# python main.py --name "dup-tabpfn-bank-n16-seed14" --cfg "cfgs/pfn/n16/bank.yaml" --seed 14 --group "tabpfn-bank-n16" -w
-# python main.py --name "dup-tabpfn-bank-n16-seed26" --cfg "cfgs/pfn/n16/bank.yaml" --seed 26 --group "tabpfn-bank-n16" -w
-# python main.py --name "dup-tabpfn-bank-n16-seed42" --cfg "cfgs/pfn/n16/bank.yaml" --seed 42 --group "tabpfn-bank-n16" -w
+python main.py --name "tabpfn-bank-n32-seed14" --cfg "cfgs/pfn/n32/bank.yaml" --seed 14 --group "tabpfn-bank-n32" -w
+python main.py --name "tabpfn-bank-n32-seed26" --cfg "cfgs/pfn/n32/bank.yaml" --seed 26 --group "tabpfn-bank-n32" -w
+python main.py --name "tabpfn-bank-n32-seed42" --cfg "cfgs/pfn/n32/bank.yaml" --seed 42 --group "tabpfn-bank-n32" -w
+python main.py --name "tabpfn-bank-n32-seed58" --cfg "cfgs/pfn/n32/bank.yaml" --seed 58 --group "tabpfn-bank-n32" -w
+python main.py --name "tabpfn-bank-n32-seed97" --cfg "cfgs/pfn/n32/bank.yaml" --seed 97 --group "tabpfn-bank-n32" -w
 
-# python main.py --name "dup-tabpfn-bank-n32-seed14" --cfg "cfgs/pfn/n32/bank.yaml" --seed 14 --group "tabpfn-bank-n32" -w
-# python main.py --name "dup-tabpfn-bank-n32-seed26" --cfg "cfgs/pfn/n32/bank.yaml" --seed 26 --group "tabpfn-bank-n32" -w
-# python main.py --name "dup-tabpfn-bank-n32-seed42" --cfg "cfgs/pfn/n32/bank.yaml" --seed 42 --group "tabpfn-bank-n32" -w
-
-
+python main.py --name "tabpfn-bank-n64-seed14" --cfg "cfgs/pfn/n64/bank.yaml" --seed 14 --group "tabpfn-bank-n64" -w
+python main.py --name "tabpfn-bank-n64-seed26" --cfg "cfgs/pfn/n64/bank.yaml" --seed 26 --group "tabpfn-bank-n64" -w
+python main.py --name "tabpfn-bank-n64-seed42" --cfg "cfgs/pfn/n64/bank.yaml" --seed 42 --group "tabpfn-bank-n64" -w
+python main.py --name "tabpfn-bank-n64-seed58" --cfg "cfgs/pfn/n64/bank.yaml" --seed 58 --group "tabpfn-bank-n64" -w
+python main.py --name "tabpfn-bank-n64-seed97" --cfg "cfgs/pfn/n64/bank.yaml" --seed 97 --group "tabpfn-bank-n64" -w
 
